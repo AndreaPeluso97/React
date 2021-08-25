332b0e246c74ae6be2c9d88124090ededeb21d82
AndreaPeluso97 <60620354+AndreaPeluso97@users.noreply.github.com>
2021-08-25 16:48:08 +0200

    First Commit

---
+++ WebSite/components/mainLayout/footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+export default class Footer extends React.Component{
+            constructor(props){
+                super(props);
+            }
+            render(){
+                return(
+                    <div>Footer</div>
+                );
+            }
+        }
